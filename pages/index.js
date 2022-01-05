fe808496f83ac4210f28879ab92851598cd320ec
DEV_97 <developer.mail97@gmail.com>
2022-01-05 10:38:28 +0100

    tittle

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app! Oke_Cuba" />
+        <Header title="This will be the Oke_Cuba oficial website. " />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
